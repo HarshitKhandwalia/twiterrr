ecb91e2c7e725553ffe22dfe805b42e8e3e68802
Harshit khandwalia <harshit8377@gmail.com>
2025-08-26 18:29:31 +0530

    DB added

--- twitter/twitter/settings.py
+++ twitter/twitter/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os 
+import os
+from signal import default_int_handler 
 
 # Try to load dotenv if available (for production)
 try:
@@ -94,10 +95,19 @@ WSGI_APPLICATION = "twitter.wsgi.application"
 
 # Always use SQLite for simplicity
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
+    "default":{
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "HOST": os.getenv("DATABASE_HOST"),
+        "PORT": os.getenv("DATABASE_PORT"),
     }
+    
 }
 
 
